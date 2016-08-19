beb1de64bd72d13b65ab7ac2100d02ad5d3d6013
Matthew Croall <phoozle@gmail.com>
2016-08-19 21:13:05 +0930

    Add "begin with letter" validator to PersonalisedPlate

--- personalised_plate.rb
+++ personalised_plate.rb
@@ -2,10 +2,8 @@ class PersonalisedPlate
   LOOKUP_URL = 'http://www.transport.sa.gov.au/ezyplates.sa.gov.au/WebContent/ezyplate.php?plate_type=P&plate='.freeze
 
   def initialize(alphanumeric)
-    if alphanumeric.length != 6
-      raise "`#{alphanumeric}` is not 6 alphaumeric charactors with at least one number"
-    end
     @alphanumeric = alphanumeric.upcase
+    validate
   end
 
   def available?
@@ -14,5 +12,17 @@ class PersonalisedPlate
 
   def lookup
     open(LOOKUP_URL + @alphanumeric).read
+  end
+
+  private
+
+  def validate
+    if @alphanumeric.length != 6
+      raise "`#{@alphanumeric}` is not 6 alphaumeric charactors with at least one number"
+    end
+
+    unless @alphanumeric[0] =~ /[a-zA-Z]/
+      raise "`#{@alphanumeric}` must begin with a letter"
+    end
   end
 end
